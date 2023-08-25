af3dc60a7ad3d84a99128662ec97a230740b38d9
newExpand <magin0auto@gmail.com>
2023-08-25 15:19:59 +0900

    몽고 DB 레포지토리 파일 추가

--- src/blog.repository.ts
+++ src/blog.repository.ts
@@ -1,3 +1,6 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Blog, BlogDocument } from './blog.schema';
 import { Injectable } from '@nestjs/common';
 import { readFile, writeFile } from 'fs/promises';
 import { PostDto } from './blog.model';
@@ -46,5 +49,38 @@ export class BlogFileRepository implements BlogRepository {
     const updatePost = { id, ...postDto, updatedDt: new Date() };
     posts[index] = updatePost;
     await writeFile(this.FILE_NAME, JSON.stringify(posts));
+  }
+}
+
+@Injectable()
+export class BlogMongoRepository implements BlogRepository {
+  constructor(@InjectModel(Blog.name) private blogModel: Model<BlogDocument>) {}
+
+  async getAllPost(): Promise<Blog[]> {
+    return await this.blogModel.find().exec();
+  }
+
+  async createPost(postDto: PostDto) {
+    const createPost = {
+      ...postDto,
+      createdDt: new Date(),
+      updatedDt: new Date(),
+    };
+
+    this.blogModel.create(createPost);
+  }
+
+  async getPost(id: string): Promise<PostDto> {
+    return await this.blogModel.findById(id);
+  }
+
+  async deletePost(id: string) {
+    await this.blogModel.findByIdAndDelete(id);
+  }
+
+  async updatePost(id: string, postDto: PostDto) {
+    const updatePost = { id, ...postDto, updatedDt: new Date() };
+
+    await this.blogModel.findByIdAndUpdate(id, updatePost);
   }
 }
