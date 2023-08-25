d5814dc454930ffff675862410103fa8e11cce05
newExpand <magin0auto@gmail.com>
2023-08-25 19:11:49 +0900

    nestjs/config 설치 후 전역 env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogController } from './blog.controll';
+import { BlogFileRepository, BlogMongoRepository } from './blog.repository';
+import { Blog, BlogSchema } from './blog.schema';
 import { BlogService } from './blog.service';
-import { BlogFileRepository } from './blog.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService, BlogFileRepository],
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
